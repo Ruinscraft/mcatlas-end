ea25f92304b067c69f57fc6c98f4bd1f58ff5e07
drewdorris <contact@dorrisd.com>
2020-07-15 18:46:27 -0400

    better

--- src/main/java/net/mcatlas/end/EndWorldCheckerTask.java
+++ src/main/java/net/mcatlas/end/EndWorldCheckerTask.java
@@ -12,27 +12,27 @@ public class EndWorldCheckerTask implements Runnable {
     private static final long DAY_LENGTH = 86400000;
     private static final long TWELVE_HOURS_LENGTH = 43200000;
 
-    private long nextCreationTime = -1L;
+    private long nextCreationTime = newCreationTime();
 
     // once a minute
     @Override
     public void run() {
         checkCreationTime();
 
-        // run task to check
+        // run task to check for world deletion
 
     }
 
     public void checkCreationTime() {
-        if (nextCreationTime == -1L) {
-            nextCreationTime = TWELVE_HOURS_LENGTH + ((int) (DAY_LENGTH * random.nextDouble()));
-        }
-
         if (System.currentTimeMillis() > this.nextCreationTime) {
             Bukkit.getLogger().info("New end world");
             // create new end world
-            nextCreationTime = -1L;
+            nextCreationTime = newCreationTime();
         }
+    }
+
+    public static long newCreationTime() {
+        return TWELVE_HOURS_LENGTH + ((int) (DAY_LENGTH * random.nextDouble()));
     }
 
 }
