e3914e5f8f712c1dac8d6c96f23651da487fd03f
drewdorris <contact@dorrisd.com>
2020-07-15 18:42:22 -0400

    change creation time system

--- src/main/java/net/mcatlas/end/EndWorldCheckerTask.java
+++ src/main/java/net/mcatlas/end/EndWorldCheckerTask.java
@@ -7,13 +7,14 @@ import java.util.Random;
 
 public class EndWorldCheckerTask implements Runnable {
 
-    private static final Calendar calendar = Calendar.getInstance();
     private static Random random = new Random();
 
     private static final long DAY_LENGTH = 86400000;
+    private static final long TWELVE_HOURS_LENGTH = 43200000;
 
     private long nextCreationTime = -1L;
 
+    // once a minute
     @Override
     public void run() {
         checkCreationTime();
@@ -23,33 +24,14 @@ public class EndWorldCheckerTask implements Runnable {
     }
 
     public void checkCreationTime() {
-        calendar.setTimeInMillis(EndPlugin.STARTUP_TIME);
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-        long currentTime = System.currentTimeMillis();
-        calendar.setTimeInMillis(currentTime);
-
-        // will not run on the same day the server has restarted to prevent it happening twice in a day
-        if (day == calendar.get(Calendar.DAY_OF_MONTH)) return;
-
-        int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        int minute = calendar.get(Calendar.MINUTE);
-
         if (nextCreationTime == -1L) {
-            calendar.setTimeInMillis(currentTime + DAY_LENGTH);
-            int newHour = (int) (23D * random.nextDouble());
-            if (newHour == 0) return; // wont run task at 12:xx AM so that two wont run at once
-            // it will run again and find a new hour
-            // could be a better solution for this
-            calendar.set(Calendar.HOUR_OF_DAY, newHour);
-            calendar.set(Calendar.MINUTE, (int) (59D * random.nextDouble()));
-
-            this.nextCreationTime = calendar.getTimeInMillis();
+            nextCreationTime = TWELVE_HOURS_LENGTH + ((int) (DAY_LENGTH * random.nextDouble()));
         }
 
-        if (currentTime > this.nextCreationTime) {
+        if (System.currentTimeMillis() > this.nextCreationTime) {
             Bukkit.getLogger().info("New end world");
             // create new end world
+            nextCreationTime = -1L;
         }
     }
 
